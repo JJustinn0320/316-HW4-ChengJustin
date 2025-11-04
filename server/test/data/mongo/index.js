939496bd2fcd878c3c8df7435a6b89bf268e76f5
Justin Cheng <justincheng0320@gmail.com>
2025-11-04 17:31:09 -0500

    moved model folder to db/mondodb/ and changed route in reset Mongo Data

--- server/test/data/mongo/index.js
+++ server/test/data/mongo/index.js
@@ -19,8 +19,8 @@ async function fillCollection(collection, collectionName, data) {
 }
 
 async function resetMongo() {
-    const Playlist = require('../../../models/playlist-model')
-    const User = require("../../../models/user-model")
+    const Playlist = require('../../../db/mongodb/models/playlist-model')
+    const User = require("../../../db/mongodb/models/user-model")
     const testData = require("../example-db-data.json")
 
     console.log("Resetting the Mongo DB")
